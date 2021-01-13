b10a11374e4ca5129e0a0c4a2e7009b267ef926c
Umang G. Patel <23169768+robonetphy@users.noreply.github.com>
2021-01-14 00:47:51 +0530

    spring backlog card support added

--- src/index.js
+++ src/index.js
@@ -1,64 +1,59 @@
-require("dotenv").config();
-const { Octokit } = require("@octokit/core");
-const axios = require("axios").default;
-const CronJob = require("cron").CronJob;
+require('dotenv').config()
+const { Octokit } = require('@octokit/core')
+const axios = require('axios').default
+const CronJob = require('cron').CronJob
 
-const TOKEN = process.env.TOKEN;
-const COLUMN_NODE_ID = process.env.COLUMN_NODE_ID;
-const NOTIFIER_URL = process.env.NOTIFIER_URL;
-const MENTION = process.env.MENTION;
-const PR_TIME = process.env.PR_TIME || "0 9,18 * * 1-5";
-const SOZVON_TIME = process.env.SOZVON_TIME || "0 21 * * 1-5";
+const TOKEN = process.env.TOKEN
+const COLUMN_NODE_ID = process.env.COLUMN_NODE_ID
+const NOTIFIER_URL = process.env.NOTIFIER_URL
+const MENTION = process.env.MENTION
+const PR_TIME = process.env.PR_TIME || '0 9,18 * * 1-5'
 
-const octokit = new Octokit({ auth: TOKEN });
+const octokit = new Octokit({ auth: TOKEN })
 
-const CARDS_QUERY = `
-query ($id: ID!) {
-  node (id: $id) {
+const MEMBERS_QUERY = `
+query {
+  organization(login: "codex-team") {
+   membersWithRole(first:30){
+    totalCount
+    nodes{
+      login
+      }
+    }
+  }
+}
+`
+const SPRING_BACKLOG_CARDS_QUERY = `
+query($id: ID!){
+  node(id: $id) {
     ... on ProjectColumn {
       name
-      cards(first: 20) {
+      cards(first: 30) {
+        __typename
         totalCount
         nodes {
+          id
+          note
+          state
+          creator {
+            login
+          }
           content {
             ... on PullRequest {
+              id
               __typename
               url
+              author {
+                login
+              }
             }
-            ... on Issue {
-              __typename
+            ... on Issue{
+              id
               url
-              timelineItems(first: 20) {
-                nodes {
-                  __typename
-                  ... on CrossReferencedEvent {
-                     source {
-                      __typename
-                      ... on PullRequest {
-                        url
-                      }
-                    }
-                    target {
-                      __typename
-                      ... on PullRequest {
-                        url
-                      }
-                    }
-                  }
-                  ... on ConnectedEvent {
-                    source {
-                      __typename
-                      ... on PullRequest {
-                        url
-                      }
-                    }
-                    subject {
-                      __typename
-                      ... on PullRequest {
-                        url
-                      }
-                    }
-                  }
+              __typename
+              assignees{
+                nodes{
+                  login
                 }
               }
             }
@@ -68,118 +63,111 @@ query ($id: ID!) {
     }
   }
 }
-`;
-
-function extractPullRequests(cards) {
-  const prs = new Set();
-  for (let card of cards) {
-    card = card.content;
-    if (!card) continue;
-    if (card.__typename === "Issue") {
-      for (const event of card.timelineItems.nodes) {
-        if (event.source) {
-          switch (event.__typename) {
-            case "ConnectedEvent":
-              prs.add(event.subject.url);
-              break;
-            case "CrossReferencedEvent":
-              prs.add(event.source.url);
-              break;
-          }
-        }
-      }
-    } else if (card.__typename === "PullRequest") {
-      prs.add(card.url);
-    }
-  }
-  return [...prs];
-}
-
-function prsToText(prs) {
-  return (
-    "Requests to review 😊\n" +
-    MENTION.split(",")
-      .map((x) => x.trim())
-      .map((x) => "@" + x)
-      .join(" ") +
-    "\n\n" +
-    prs.join("\n")
-  );
-}
+`
 
 async function notify(data) {
   return axios({
-    method: "POST",
+    method: 'POST',
     url: NOTIFIER_URL,
-    data: "message=" + encodeURIComponent(data)
+    data: 'message=' + encodeURIComponent(data)
   })
 }
 
-async function sendOpenPrs() {
-  const query = await octokit.graphql(CARDS_QUERY, { id: COLUMN_NODE_ID });
-  console.log(query);
-  console.log(query.node.cards.nodes);
-  const prs = extractPullRequests(query.node.cards.nodes);
-  console.log(prs);
-
-  const resp = await axios({
-    method: "POST",
-    url: NOTIFIER_URL,
-    data: "message=" + encodeURIComponent(prsToText(prs)),
-  });
-  console.log(resp.status);
-  console.log(resp.data);
-}
-
-function check() {
+function parseBacklogCard(members) {
   return octokit
-    .graphql(CARDS_QUERY, { id: COLUMN_NODE_ID })
+    .graphql(SPRING_BACKLOG_CARDS_QUERY, { id: COLUMN_NODE_ID })
     .then((query) => {
-      console.log(query);
-      console.log(query.node.cards.nodes);
-      console.info(
-        "Preview:\n" + prsToText(extractPullRequests(query.node.cards.nodes))
-      );
+      const data = query.node.cards.nodes.map((items) => {
+        if (items.state === 'NOTE_ONLY') {
+          return (items.note)
+        }
+        if (items.state === 'CONTENT_ONLY') {
+          if (items.content.__typename === 'PullRequest') { return ('@' + items.content.author.login + ' ' + items.content.url) }
+          if (items.content.__typename === 'Issue') {
+            const people = items.content.assignees.nodes.map((item) => {
+              return '@' + item.login + ' '
+            })
+            return (people + ' ' + items.content.url)
+          }
+        }
+      })
+      let processed = [...data]
+      for (let i = 0; i < members.length; i++) {
+        processed = processed.map((x) => x.replace(new RegExp('@' + members[i].name, 'g'), ''))
+      }
+      processed = processed.map((x) => x.replace(/(\r\n|\n|\r)/gm, ''))
+      data.map((items, index) => {
+        for (let i = 0; i < members.length; i++) {
+          if (items.includes('@' + members[i].name)) {
+            members[i].tasks.push(processed[index])
+          }
+        }
+      })
+      return members
     })
     .catch((err) => {
-      console.error("Can't make test request\n", err);
-      process.exit(1);
-    });
+      console.error("Can't make test request for cards\n", err)
+      process.exit(1)
+    })
+}
+const getMembersName = (memberList) => {
+  const members = []
+  if (memberList) {
+    memberList.split(' ').map((items) => {
+      members.push({
+        name: items,
+        tasks: []
+      })
+    })
+    return members
+  }
+  return octokit
+    .graphql(MEMBERS_QUERY)
+    .then((query) => {
+      query.organization.membersWithRole.nodes.map((items) => {
+        members.push({
+          name: items.login,
+          tasks: []
+        })
+      })
+      return members
+    })
+    .catch((err) => {
+      console.error("Can't make test request for Members\n", err)
+      process.exit(1)
+    })
 }
 
-const SOZVON_MSG = `☝️
-Join the meeting in Discord!
-@specc @guryn @khaydarovm @nikmel2803 @gohabereg @ilyamore88 @GeekaN @augustovich @n0str @f0m41h4u7 @polina_shneider @oybekmuslimov @xemk4`
 
 async function main() {
-  await check();
-  console.log("Successful check, continuing");
   const job = new CronJob(
     PR_TIME,
     () => {
-      console.log("Firing pr job");
-      sendOpenPrs()
-        .then(() => console.log("Job completed"))
-        .catch(console.error);
+      console.log('Firing pr job');
+      (async () => {
+        let dataToSend = "📌 Sprint's backlog \n\n"
+        const response = await parseBacklogCard(await getMembersName(MENTION))
+        response.map((items) => {
+          dataToSend += ('@' + items.name)
+          dataToSend += '\n'
+          items.tasks.map((data) => {
+            dataToSend += ('⚡️ ' + data + '\n')
+          })
+          dataToSend += '\n\n'
+        })
+        notify(dataToSend)
+      })()
+        .then(() => console.log('Job completed'))
+        .catch(console.error)
     },
     null,
     true,
-    "Europe/Moscow"
-  );
-  job.start();
-  console.log("Notifier started");
-  console.log("Will notify at " + PR_TIME);
+    'Europe/Moscow'
+  )
+  job.start()
+  console.log('Notifier started')
+  console.log('Will notify at ' + PR_TIME)
 
-  const sozvonJob = new CronJob(SOZVON_TIME, () => {
-    console.log("Firing sozvon job");
-    notify(SOZVON_MSG)
-      .then(() => console.log("OK"))
-      .catch(console.error)   
-  }, null, true, "Europe/Moscow")
-  sozvonJob.start();
-  console.log("Sozvon started");
-  console.log("Will notify at " + SOZVON_TIME);
-  notify("👨‍🌾PR + Sozvon bot started").then(() => "Notify to chat OK").catch(console.error);
 }
 
-main();
+main()
